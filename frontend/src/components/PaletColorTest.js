a3b6c4c47d5fec8d4797a28ae2a5cf4169dc344e
aaronma300604 <aaronmayoralansias@gmail.com>
2024-10-17 15:32:12 +0200

    Added colors.css where we can define a color palet. Added test screen and css to tes color palets

---
+++ frontend/src/components/PaletColorTest.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "../static/css/components/testpalet.css"
+import GameContainer from './GameContainer';
+import { Button, ButtonGroup } from "reactstrap";
+
+export default function PaletColorTest(){
+
+    return(
+        <div>
+        <ul className = "current-games-container"> 
+            <GameContainer key = {25555} gameCode = {25555} gameMode = "Team Battle" state = "Waiting players" host = "lfb9498" playerNumber = {8} maxPlayers = {8} spectatorsNumber = {8} isPrivate = {true} />
+            <GameContainer key= {30315} gameCode = {30315} gameMode = "Versus" state = "In progress" host = "lfb9498" playerNumber = {8} maxPlayers = {8} spectatorsNumber = {8} isPrivate = {false}/>
+        </ul>
+        <ButtonGroup className="test-button-group">
+            <Button size="lg" className="test-button">Test 1</Button>
+            <Button size="lg" className="test-button2">Test 2</Button>
+        </ButtonGroup>
+        </div>
+    );
+
+}
