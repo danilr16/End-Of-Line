dd4a31d4cb10155bf3b5c82b8de34c099146f6c1
aaronma300604 <aaronmayoralansias@gmail.com>
2024-10-29 13:09:26 +0100

    Refactored DnD system

--- frontend/src/components/GameCard.js
+++ frontend/src/components/GameCard.js
@@ -1,26 +1,58 @@
 import React from 'react';
 import classNames from "classnames";
-import { GameCardIcon } from './GameCardIcon';
+import { useEffect,useState,useRef } from 'react';
+import { CardOverlay } from './CardOverlay';
 
-
+//Carta invisible, setea la posicion del elemento visual
 const GameCard = ({ size, iconName, hoverable = true , beingDraggedCard, setBeingDraggedCard,index}) => {
+    const cardRef = useRef(null);
+    const [position, setPosition] = useState({top:0,left:0})
+
+    const [mousePosition, setMousePosition] = useState({ top: 0, left: 0 });
+
+    useEffect(() => {
+    const handleMouseMove = (event) => {
+        setMousePosition({ left: event.clientX, top: event.clientY });
+    };
+
+      // Attach the event listener to the window object
+    window.addEventListener('mousemove', handleMouseMove);
+
+      // Cleanup function to remove the event listener
+    return () => {
+        window.removeEventListener('mousemove', handleMouseMove);
+    };
+    }, []);
+
 
     
     const handleDragStart = () => {setBeingDraggedCard(index)}
     const handleDragEnd = () => {setBeingDraggedCard(null)}
 
+    useEffect(() => {
+        setTimeout(()=>{
+            if (cardRef.current) {
+            const rect = cardRef.current.getBoundingClientRect();
+            setPosition({top:rect.top+10,left:rect.left});
+            console.log(rect.top,rect.left)
+        }},30)
+
+    }, []);
+
 
     return (
+        <React.Fragment>
         <div 
+            ref={cardRef}
             className={classNames({'game-card-container':true, 'non-hoverable':!hoverable, 'dragging': beingDraggedCard&& beingDraggedCard === index})}
             draggable
             onDragStart={handleDragStart}
             onDragEnd={handleDragEnd}
-            style={{ minWidth: `${size}px`, minHeight: `${size}px`, position: 'relative' }}
-
-        >
-            {iconName && <GameCardIcon iconName={iconName}/>}
-        </div>
+            style={{ minWidth: `${size}px`, minHeight: `${size}px`}}
+        />
+        <CardOverlay iconName={iconName} size = {size} position= {beingDraggedCard?mousePosition:position}/>
+        </React.Fragment>
+        
     );
 };
 
