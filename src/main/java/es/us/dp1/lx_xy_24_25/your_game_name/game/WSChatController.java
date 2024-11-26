cdff8ab802c077def103a685579f8a4d3176ae1f
aaronma300604 <aaronmayoralansias@gmail.com>
2024-11-26 12:30:35 +0100

    Fixing WSChat

--- src/main/java/es/us/dp1/lx_xy_24_25/your_game_name/game/WSChatController.java
+++ src/main/java/es/us/dp1/lx_xy_24_25/your_game_name/game/WSChatController.java
@@ -4,15 +4,18 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import es.us.dp1.lx_xy_24_25.your_game_name.exceptions.AccessDeniedException;
+
 @Controller
 public class WSChatController {
 
     @Autowired
     GameService gs;
-    
+
     @MessageMapping("/chat")
     @SendTo("/topic/chat") 
     public ChatMessage send(ChatMessage cm) {
+        if(!cm.getJwtu().validateJwtToken(cm.getJwt())) throw new AccessDeniedException("You can't type in this chat room");
         gs.sendChatMessage(cm);
         return cm;
     }
