45ac50f1501a6aaf47c8bf3ff33e0e5aba617dc7
Ivanmbus <ivamirbal@alum.us.es>
2025-01-07 19:32:48 +0100

    chages in myGames screen

--- frontend/src/components/MyGameModal.js
+++ frontend/src/components/MyGameModal.js
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function MyGameModal({selectedGame, closeGameDataModal, parseGamemode, winLost}) {
 
     const modalClass = winLost === "WON" 
@@ -6,38 +8,70 @@ export default function MyGameModal({selectedGame, closeGameDataModal, parseGame
     ? "modal-lost" 
     : "modal-neutral";
 
+    const modalClassButton = winLost === "WON" 
+    ? "modal-bw" 
+    : winLost === "LOST" 
+    ? "modal-bl" 
+    : "modal-neutral";
+
+    const modalWL = winLost === "WON" 
+    ? "modal-color-won" 
+    : winLost === "LOST" 
+    ? "modal-color-lost" 
+    : "modal-neutral";
+
+    const win_o_lost = winLost === "WON" 
+    ? true
+    : winLost === "LOST" ? false : null;
+
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate(`/game/${selectedGame.gameCode}`);
+    };
+    
+
     return (
         <div className="modal-overlay" onClick={closeGameDataModal}>
-            <div 
-                className={`modal-container-MyGames ${modalClass}`} 
+            <div
+                className={`modal-container-MyGames ${modalClass}`}
                 onClick={(e) => e.stopPropagation()}
             >
                 <h2 className="modal-title">Game Details</h2>
+                <h3 className={"modal-title"}>{win_o_lost ? "You Won!" : "You Lost!"}</h3>
                 {selectedGame ? (
+                <>
                     <div style={{ textAlign: 'left' }}>
-                        <p style={{ marginTop: '20px' }}>
-                            <strong>Game Mode:</strong> {parseGamemode}
-                        </p>
-                        <p>
-                            <strong>Players:</strong>
-                        </p>
-                        {/* Contenedor para los jugadores */}
-                        <div className="players-columns">
-                            {selectedGame.players.map((player, index) => (
-                                <div className="player-item" key={index}>
-                                    <img 
-                                        src={player.user.image} 
-                                        alt="Player" 
-                                        className="small-profile-image" 
-                                    />
-                                    <span>{player.user.username}</span>
-                                </div>
-                            ))}
+                    <p style={{ marginTop: '20px' }}>
+                        <strong style={{color:'black' }}>Game Mode:</strong> {parseGamemode}
+                    </p>
+                    <p>
+                        <strong style={{color:'black' }}>Players:</strong>
+                    </p>
+                    {/* Contenedor para los jugadores */}
+                    <div className="players-columns">
+                        {selectedGame.players.map((player, index) => (
+                        <div className="player-item" key={index}>
+                            <img
+                            src={player.user.image}
+                            alt="Player"
+                            className="modal-image"
+                            />
+                            <span>{player.user.username}</span>
                         </div>
-
-                        <button className={`rounded-button ${modalClass}`} onClick={closeGameDataModal}> Final Game State </button>
-
+                        ))}
                     </div>
+                    </div>
+                    {/* Botón centrado */}
+                    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                    <button
+                        className={`rounded-button ${modalClassButton}`}
+                        onClick={handleButtonClick}
+                    >
+                        Final Game State
+                    </button>
+                    </div>
+                </>
                 ) : (
                     <p>No game data available.</p>
                 )}
